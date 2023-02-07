47ddc37f6b26de2e77cee03c332ee67ddf91063c
Jalaj Kumar Gaur <raajk0338@gmail.com>
2023-02-08 01:10:22 +0530

    lc

--- src/routes/Author.ts
+++ src/routes/Author.ts
@@ -4,10 +4,18 @@ import { Schemas, validateSchema } from "../middleware/validateSchema";
 
 const router = express.Router();
 
-router.post("/create", validateSchema(Schemas.author.create), controlller.createAuthor);
+router.post(
+  "/create",
+  validateSchema(Schemas.author.create),
+  controlller.createAuthor
+);
 router.get("/get/:authorId", controlller.readAuthor);
 router.get("/get/", controlller.readAllAuthor);
-router.patch("/update/:authorId", validateSchema(Schemas.author.update), controlller.UpdateAuthor);
+router.patch(
+  "/update/:authorId",
+  validateSchema(Schemas.author.update),
+  controlller.UpdateAuthor
+);
 router.delete("/delete/:authorId", controlller.DeleteAuthor);
 
 export = router;
